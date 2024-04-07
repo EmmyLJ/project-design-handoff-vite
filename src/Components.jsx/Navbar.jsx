e03308670f8a1851db2c7fcc0ea33419520a9fcf
Emmy Jansson <emmybengtsson@live.se>
2024-04-07 10:58:43 +0200

    finally time to update my repository on github as well

--- src/Components.jsx/Navbar.jsx
+++ src/Components.jsx/Navbar.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
 import "../translations";
@@ -5,10 +6,19 @@ import { useTranslation } from "react-i18next";
 
 export const Navbar = () => {
   const { t, i18n } = useTranslation();
+  const [isOpen, setIsOpen] = useState(false);
 
   const toggleLanguage = () => {
     const newLanguage = i18n.language === "en" ? "sv" : "en";
     i18n.changeLanguage(newLanguage);
+  };
+
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen); // Toggle the isOpen state
+  };
+
+  const handleLinkClick = () => {
+    setIsOpen(false); // Close the dropdown when a link is clicked
   };
 
   return (
@@ -31,6 +41,52 @@ export const Navbar = () => {
           <button className="sign-in-button">{t("signIn")}</button>
           <button onClick={toggleLanguage}>Swedish/English</button>
         </div>
+        <img
+          src="./hamburger.svg"
+          alt="Hamburger"
+          className="hamburger"
+          onClick={toggleDropdown}
+        />
+        {isOpen && (
+          <div className="dropdown">
+            <Link
+              to="/about"
+              className="dropdown-item"
+              onClick={handleLinkClick}
+            >
+              About
+            </Link>
+            <Link
+              to="/membership"
+              className="dropdown-item"
+              onClick={handleLinkClick}
+            >
+              Membership
+            </Link>
+            <Link
+              to="/facilities"
+              className="dropdown-item"
+              onClick={handleLinkClick}
+            >
+              Facilities
+            </Link>
+            <Link
+              to="/opening-hours"
+              className="dropdown-item"
+              onClick={handleLinkClick}
+            >
+              Opening Hours
+            </Link>
+            <Link
+              to="/sign-in"
+              className="dropdown-item"
+              onClick={handleLinkClick}
+            >
+              Sign in
+            </Link>
+            <button onClick={toggleLanguage}>Swedish/English</button>
+          </div>
+        )}
       </nav>
     </header>
   );
